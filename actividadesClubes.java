b1b2a9a0836628a1b873595ec885cc1517b75dfe
Dario Fuentes <dariofp123@gmail.com>
2024-08-24 22:43:43 -0400

    Se añadieron constructores y metodos adicionales con sobrecarga a las clases "clubesDeportivos" y "actividadesClubes"

--- actividadesClubes.java
+++ actividadesClubes.java
@@ -1,13 +1,53 @@
 import java.util.*;
 
-public class actividadesClubes {
-    private String actividad;
-    private String descripcion;
-    private int idActividad;
-    private String horario;
+public class actividadesClubes{
+    private int idActividad; //Identificador de la actividad.
+    private String actividad; // Nombre de la actividad.
+    private String descripcion; // Descripción de la actividad.
+    private String horario; 
     private String lugar;
 
-    // getters
+    //Constructor default.
+    public actividadesClubes()
+    {
+        this.idActividad = 0;
+        this.actividad = "";
+        this.descripcion = "";
+        this.horario = "";
+        this.lugar = "";
+    }
+
+    //Constructor con tres parametros basicos. (Id, actividad,horario)
+    public actividadesClubes(int idActividad, String actividad, String horario)
+    {
+        this.idActividad = idActividad;
+        this.actividad = actividad;
+        this.horario = horario;
+        this.descripcion = "";
+        this.lugar = "";
+    }
+
+    //Constrictor con todos los parametros.
+    public actividadesClubes(int idActividad, String actividad, String horario, String descripcion, String lugar)
+    {
+        this.idActividad = idActividad;
+        this.actividad = actividad;
+        this.horario = horario;
+        this.descripcion = descripcion;
+        this.lugar = lugar;
+    }
+
+    //Constructor que copia datos de una actividad ya existente.
+    public actividadesClubes(actividadesClubes actividad)
+    {
+        this.idActividad = actividad.idActividad;
+        this.actividad = actividad.actividad;
+        this.descripcion = actividad.descripcion;
+        this.lugar = actividad.lugar;
+        this.horario = actividad.horario;
+    } 
+
+    //Getters
     public String getActividad() {
         return actividad;
     }
@@ -26,8 +66,7 @@ public class actividadesClubes {
         return lugar;
     }   
 
-    // setters
-
+    //Setters
     public void setActividad(String actividad) {
         this.actividad = actividad;
     }
@@ -48,5 +87,49 @@ public class actividadesClubes {
         this.lugar = lugar;
     }
 
+    //Metodos adicionales.
 
+    //Establecer el ID de la actividad validando que sea positivo.
+    public void setID(int id, boolean validar)
+    {
+        if(validar && id <= 0)
+        {
+            throw new IllegalArgumentException("El ID de la actividad debe ser positivo.")
+        }
+        this.idActividad = id;
+    }
+
+    //Establecer el nombre de la actividad en mayusculas si se desea.
+    public void setActividad(String actividad, boolean mayuscula)
+    {
+        if(mayuscula)
+        {
+            this.actividad = actividad.toUpperCase();
+        }
+        else{
+            this.actividad = actividad;
+        }
+    }
+
+    //Establecer el lugar de la actividad añadiendo su numero de sala.
+    public void setLugar(String lugar, int sala)
+    {
+        if(sala <= 0){
+            throw new IllegalArgumentException("El número de la sala debe ser positivo")
+        }
+        this.lugar = lugar + " - Sala número" + sala;
+    }
+
+    //Establecer la descripcion en mayusculas si se desea.
+    public void setDescripcion(String descripcion, boolean mayus)
+    {
+        if(mayus)
+        {
+            this.descripcion = descripcion.toUpperCase();
+        }
+        else{
+            this.descripcion = descripcion;
+        }
+    }
+    
 }
--- clubesDeportivos.java
+++ clubesDeportivos.java
@@ -1,27 +1,115 @@
 import java.util.*;
-public class clubesDeportivos {
+public class clubesDeportivos{
+    private int idClub; //identificador del club
     private String nombre; //nombre del club
     private String direccion; //direccion donde se encuentra el club
-    private int idClub; //identificador del club
     private ArrayList<actividadesClubes> actividades; //actividades cuales tiene el club
-    private ArrayList<String> Socios; //Socios a quienes les pertenecen el club
+    private ArrayList<String> socios; //Socios a quienes les pertenecen el club
 
-    // getters
+    // Constructor default.
+    public clubesDeportivos()
+    {
+        this.nombre = "";
+        this.direccion = "";
+        this.idClub = 0;
+        this.actividades = new ArrayList<>();
+        this.socios = new ArrayList<>();
+    }
 
+    // Constructor con parametros basicos (idClub, nombre, direccion)
+    public clubesDeportivos(int idClub, String nombre, String direccion)
+    {
+        this.idClub = idClub;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.actividades = new ArrayList<>();
+        this.socios = new ArrayList<>();
+    }
+
+    // Constructor con todos los parametros.
+    public clubesDeportivos(int idClub, String nombre, String direccion, ArrayList<actividadesClubes> actvidades, ArrayList<String> socios)
+    {
+        this.idClub = idClub;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.actividades = actvidades;
+        this.socios = socios;
+
+    }
+
+    // Constructor que copia datos ya de un club ya existente.
+    public clubesDeportivos(clubesDeportivos club)
+    {
+        this.idClub = club.idClub;
+        this.nombre = club.nombre;
+        this.direccion = club.direccion;
+        this.actividades = new ArrayList<>(club.actividades);
+        this.socios = new ArrayList<>(club.socios);
+    }
+
+    // Getters
     public String getNombre(){return nombre;}
     public String getDireccion(){return direccion;}
     public int getidClub(){return idClub;}
     public ArrayList<actividadesClubes> getActividades(){return actividades;}
-    public ArrayList<String> getSocios(){return Socios;}
+    public ArrayList<String> getSocios(){return socios;}
 
-    //setters
-
+    // Setters
     public void setNombre(String nombre){this.nombre = nombre;}
     public void setDireccion(String direccion){this.direccion = direccion;}
-    public void setID(int id){this.idClub = id;}
+    public void setId(int id){this.idClub = id;}
     public void setActividades(ArrayList<actividadesClubes> actividades) {this.actividades = actividades;}
-    public void setSocios(ArrayList<String> Socios){this.Socios = Socios;}
+    public void setSocios(ArrayList<String> Socios){this.socios = Socios;}
 
 
+    // Metodos adicionales.
 
+    // Establecer el Id del club validando que sea positivo.
+    public void setId(int id, boolean validar)
+    {
+        if(validar && id <= 0)
+        {
+            throw new IllegalArgumentException("El ID del club debe ser positivo.") 
+        }
+        this.idClub = id;
+    }
+
+    // Establecer el nombre del club en mayusculas si se desea.
+    public void setNombre(String nombre, boolean mayuscula)
+    {
+        if(mayuscula == true)
+        {
+            this.nombre = nombre.toUpperCase();
+        }
+        else{
+            this.nombre = nombre;
+        }
+    }
+
+    // Establecer la direccion del club añadiendo su comuna.
+    public void setDireccion(String direccion, String comuna)
+    {
+        this.direccion = direccion + ", " + comuna;
+    }
+
+    // Añadir un socio en especifico.
+    public void addSocio(String socio)
+    {
+        if(this.socios == null)
+        {
+            this.socios = new ArrayList<>();
+        }
+        this.socios.add(socio);
+    }
+
+    // Añadir multiples socios desde un arreglo.
+    public void addSocio(String[] sociosA)
+    {
+        if(this.socios == null)
+        {
+            this.socios = new ArrayList<>();
+        }
+        this.socios.addAll(Arrays.asList(sociosA));
+    }
+    
 }
